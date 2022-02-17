f753cde10756fe289b678f1603ff678d834832e8
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-02-17 16:26:14 +0530

    Time: 5 ms (13.63%), Space: 5.9 MB (70.90%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
+        // Total number of possible moves. means m-1 in down and n-1 in right.
         int N = m + n -2;
         int r = m - 1;
         double res = 1;
-        
+        // Here our probelm breaks into permutation so we simply find permutaion.
         for(int i = 1; i <= r; i++){
             res = res * (N - r + i) / i;
         }
