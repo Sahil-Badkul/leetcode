f6dc94ed264b62778bd39e9cac5228c42ee64456
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-02-17 20:42:46 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (69.45%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n < 1) return false;
-        if(n == 1) return true;
-        while(n%2 == 0) n /= 2;
-        return n == 1;
+        if(n < 1) return 0;
+        return ((n&(n-1)) == 0);
     }
 };
