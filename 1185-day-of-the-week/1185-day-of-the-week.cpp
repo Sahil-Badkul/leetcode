02115580f8d1ba747ee721bf628520f19bcb2f94
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-02-27 14:26:09 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (86.93%) - LeetHub

--- 1185-day-of-the-week/1185-day-of-the-week.cpp
+++ 1185-day-of-the-week/1185-day-of-the-week.cpp
@@ -12,22 +12,28 @@ public:
     }
     string dayOfTheWeek(int day, int month, int year) {
         vector<string> days = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-        vector<int> monthOdd = {3,0,3,2,3,2,3,3,2,3,2,3};
-        // year = 1998
+        vector<int> monthOdd = {3,0,3,2,3,2,3,3,2,3,2,3};       // oddDays in months.
+        /*
+        1998 => 1600 + 300 + 38;
+        1600 => 0
+        300 => 1
+        38 => 38 + 9 = 48
+        48 % 7 => 6;
+        */
         int mod400 = (year-1)%400;  // 338
         int mod100 = (mod400%100);  // 38
-        int by100 = ((mod400/100) * 5) % 7; // calculate extra for cent = 1
+        int by100 = ((mod400/100) * 5) % 7; // calculate extra for century = 1
         int by4 = mod100 + mod100/4;    // calculate extra for year => 48
-        int totalExtra = (by100 + by4)%7;  // total extra
-        int preMonth = 0;
+        int totalExtraYear = (by100 + by4)%7;  // total extra
+        int preMonth = 0;   // calculate previus odd month;
         if(isLeap(year)){
             monthOdd[1] = 1;
         }
         for(int i = 0; i < month-1; i++){
             preMonth += monthOdd[i];
         }
-        int extra = (totalExtra + preMonth + day) % 7;
-        return days[extra];
+        int oddDays = (totalExtraYear + preMonth + day) % 7;
+        return days[oddDays];
         
     }
 };
