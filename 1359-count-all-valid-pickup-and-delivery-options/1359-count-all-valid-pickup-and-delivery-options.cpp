81a95512149ed904a8286a2f7e883e099397a35b
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-03-06 07:09:54 +0530

    Time: 4 ms (44.03%), Space: 6 MB (53.14%) - LeetHub

---
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countOrders(int n) {
+        // factoria * (2n-1);
+        long long res = 1;
+        int M = 1e9+7;
+        for(int i = 2; i <= n; i++){
+            res = (res*i) % M;
+        }
+        for(int i = 1; i < 2*n; i+=2){
+            res = (res*i) % M;
+        }
+        return (int) res%M;
+    }
+    
+};
