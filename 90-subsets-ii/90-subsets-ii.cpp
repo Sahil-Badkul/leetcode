17d29ff9fd1bfeae68523a5e6a5e13446789e629
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-03-10 18:42:47 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (89.24%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
-    void uniqSub(int idx, vector<int>&arr, vector<int> &ds, set<vector<int>> &res){
-        if(idx == arr.size()){
-            sort(ds.begin(), ds.end());
-            res.insert(ds);
-            return;
+    // void uniqSub(int idx, vector<int>&arr, vector<int> &ds, vector<vector<int>> &res){
+    //     if(idx == arr.size()){
+    //         sort(ds.begin(), ds.end());
+    //         res.insert(ds);
+    //         return;
+    //     }
+    //     uniqSub(idx+1, arr, ds, res);
+    //     ds.push_back(arr[idx]);
+    //     uniqSub(idx+1, arr, ds, res);
+    //     ds.pop_back();
+    // }
+    void uniqSub(int idx, vector<int> &arr, vector<int>&ds, vector<vector<int>> &ans){
+        ans.push_back(ds);
+        for(int i = idx; i < arr.size(); i++){
+            if(i == idx || arr[i] != arr[i-1]){
+                ds.push_back(arr[i]);
+                uniqSub(i+1, arr, ds, ans);
+                ds.pop_back();
+            }
         }
-        uniqSub(idx+1, arr, ds, res);
-        ds.push_back(arr[idx]);
-        uniqSub(idx+1, arr, ds, res);
-        ds.pop_back();
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        set<vector<int>> res;
-        vector<int> ds;
-        uniqSub(0, nums, ds, res);
         vector<vector<int>> ans;
-        for(auto &subset : res){
-            ans.push_back(subset);
-        }
+        vector<int> ds;
+        uniqSub(0, nums, ds, ans);
         return ans;
     }
 };
