6be2bc01cd3934a3dd094016fc8b05fc6b1655d2
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-03-13 07:15:29 +0530

    Time: 3 ms (35.76%), Space: 6.5 MB (10.36%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -2,27 +2,18 @@ class Solution {
 public:
     bool isValid(string s) {
         unordered_map<char,int> umap = { {'(',1}, {')',-1}, {'{',2}, {'}',-2}, {'[',3}, {']',-3}};
-
-        int n = s.size();        
+        
         stack<char> st;
-        for(int i = 0; i < n; i++){
-            if(umap[s[i]] > 0){
-                st.push(s[i]);
-            }else if(umap[s[i]] < 0){
-                if(!st.empty()){
-                    char c = st.top();
-                    if(umap[s[i]] + umap[c] != 0){
-                        return false;
-                    }
-                    st.pop();
-                }else{
-                    return false;
-                }
+        for(auto &c : s){
+            if(umap[c] > 0){
+                st.push(c);
+            }else{
+                if(st.empty()) return false;
+                if(umap[st.top()] + umap[c] != 0) return false;
+                st.pop();
             }
         }
-        if(st.empty()){
-            return true;
-        }
-        return false;
+        if(!st.empty()) return false;
+        return true;
     }
 };
