bf165a76ce8578eb4e4551cda90e5dddb677f10b
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-03-15 08:51:36 +0530

    Time: 12 ms (60.36%), Space: 15.4 MB (55.43%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,5 +1,24 @@
 class Solution {
 public:
+    // Using Two Pointer
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int i = 0;
+        int j = numbers.size()-1;
+        while(i < j){
+            int sum = numbers[i]+numbers[j];
+            if(sum > target){
+                j--;
+            }else if(sum < target){
+                i++;
+            }else{
+                return {i+1, j+1};
+            }
+        }
+        // not found 
+        return {};
+    }
+    /*
+    // Using binary search
     int binSearch(vector<int> &arr,int key, int l, int r){
         while(l <= r){
             int mid = l + (r-l)/2;
@@ -23,4 +42,6 @@ public:
         }
         return {-1,-1};
     }
+    */
+    
 };
