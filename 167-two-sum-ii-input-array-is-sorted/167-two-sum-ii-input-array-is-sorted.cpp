783d4480b21ea6957c8f1f0d6af01b586174f0b0
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-03-15 08:47:08 +0530

    Time: 27 ms (10.23%), Space: 15.7 MB (26.20%) - LeetHub

---
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int binSearch(vector<int> &arr,int key, int l, int r){
+        while(l <= r){
+            int mid = l + (r-l)/2;
+            if(arr[mid] < key){
+                l = mid+1;
+            }else if(arr[mid] > key){
+                r = mid-1;
+            }else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int n = numbers.size();
+        for(int i = 0; i < n-1; i++){
+            int s = binSearch(numbers, target-numbers[i], i+1, n-1);
+            if(s != -1){
+                return {i+1, s+1};
+            }
+        }
+        return {-1,-1};
+    }
+};
