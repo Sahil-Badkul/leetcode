f074af9e83d4c522179be7eb060deee718dbc4ff
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-03-18 07:52:55 +0530

    Time: 9 ms (65.56%), Space: 7.3 MB (41.53%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -2,24 +2,26 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
         vector<int> s1hash(26,0), s2hash(26,0);
-        int s1size = s1.size();
-        int s2size = s2.size();
-        if(s1size > s2size) return false;
-        int left = 0, right = 0;
-        while(right < s1size){
-            s1hash[s1[right] - 'a']++;
-            s2hash[s2[right] - 'a']++;
-            right++;
+        if(s1.size() > s2.size()){
+            return false;
         }
-        right--;
-        while(right < s2size){
-            if(s1hash == s2hash) return true;
-            right++;
-            if(right != s2size){
-                s2hash[s2[right] - 'a']++;
+        int l = 0, r = 0;
+        while(r < s1.size()){
+            s1hash[s1[r] - 'a']++;
+            s2hash[s2[r] - 'a']++;
+            r++;
+        }
+        r--;
+        while(r < s2.size()){
+            if(s1hash == s2hash){
+                return true;
             }
-            s2hash[s2[left] - 'a']--;
-            left++;
+            r++;
+            if(r != s2.size()){
+                s2hash[s2[r]-'a']++;
+            }
+            s2hash[s2[l]-'a']--;
+            l++;
         }
         return false;
     }
