94150754fad47b88b1fca60ded875f2759a5f2f5
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-03-19 10:39:30 +0530

    Time: 343 ms (31.83%), Space: 96.8 MB (15.87%) - LeetHub

--- 895-maximum-frequency-stack/895-maximum-frequency-stack.cpp
+++ 895-maximum-frequency-stack/895-maximum-frequency-stack.cpp
@@ -1,25 +1,25 @@
 class FreqStack {
 public:
-    int maxFreq;
-    unordered_map<int,int> mp;
-    unordered_map<int, stack<int>> mpSt;
+    int maxFreq;        // For reference to maximum frequent element
+    unordered_map<int,int> mp;      // For storing the freq of an element
+    unordered_map<int, stack<int>> mpSt;    // For storing occurance of all same freq element like 3,4,6,7 all have same freq.
     FreqStack() {
         maxFreq = 0;
     }
     void push(int x) {
-        mp[x]++;
-        if(maxFreq < mp[x]) maxFreq = mp[x];
-        mpSt[mp[x]].push(x);
+        mp[x]++;        // updating element freq.
+        if(maxFreq < mp[x]) maxFreq = mp[x];    // updating max freq.
+        mpSt[mp[x]].push(x);    // updating stack with same freq.
     }
     int pop() {
-        int currTop = mpSt[maxFreq].top();
-        mpSt[maxFreq].pop();
-        mp[currTop]--;
-        if(mpSt[maxFreq].empty()){
+        int currTop = mpSt[maxFreq].top();  // picking most recent element with max freq.
+        mpSt[maxFreq].pop();    // deleting that.
+        mp[currTop]--; 
+        if(mpSt[maxFreq].empty()){      // If stack is empty then remove key related to that stack.
             mpSt.erase(maxFreq);
             maxFreq--;
         }
-        return currTop;
+        return currTop;  
     }
 };
 
