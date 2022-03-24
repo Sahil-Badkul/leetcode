61ed9804f81c9635a31065b871ca102c622a833c
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-03-24 18:14:40 +0530

    Time: 0 ms (100.00%), Space: 6 MB (55.14%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n <= 0) return 0;
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+        int oneBack = 2;
+        int twoBack = 1;
+        int allstep;
+        for(int i = 3; i <= n; i++){
+            allstep = oneBack + twoBack;
+            twoBack = oneBack;
+            oneBack = allstep;
+        }
+        return allstep;
+    }
+};
