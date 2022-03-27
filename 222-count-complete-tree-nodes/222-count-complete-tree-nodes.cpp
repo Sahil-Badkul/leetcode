75f760c8287aa0c7a7c2627586beec25294b53f3
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-03-27 18:10:27 +0530

    Time: 41 ms (63.24%), Space: 30.8 MB (64.09%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
@@ -11,6 +11,7 @@
  */
 class Solution {
 public:
+    // go left till we can. TC:log(n);
     int findleftheight(TreeNode *node){
         int h = 0;
         while(node){
@@ -19,6 +20,7 @@ public:
         }
         return h;
     }
+    // go right till we can
     int findrightheight(TreeNode* node){
         int h = 0;
         while(node){
@@ -29,9 +31,14 @@ public:
     }
     int countNodes(TreeNode* root) {
         if(root == NULL) return 0;
+        // Calculating left and right height;
         int lh = 1+findleftheight(root->left);
         int rh = 1+findrightheight(root->right);
+        // if both are same mean it's a complete BT.
+        // No. of node are 2^n-1
         if(lh == rh) return (1<<lh) -1;
+        // else
         return 1 + countNodes(root->left) + countNodes(root->right);
     }
+    // overall TC : O(logn);
 };
