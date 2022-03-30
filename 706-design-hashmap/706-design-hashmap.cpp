dc54ae032903fc692d6910736f7e354fcab01204
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-03-30 07:05:14 +0530

    Time: 128 ms (87.77%), Space: 208.2 MB (15.93%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.cpp
+++ 706-design-hashmap/706-design-hashmap.cpp
@@ -1,19 +1,20 @@
 class MyHashMap {
 public:
-    map<int, int> mp;
-    MyHashMap() {}
+    int mp[1000001];
+    MyHashMap() {
+        fill(mp,mp+1000001,-1);
+    }
     
     void put(int key, int value) {
         mp[key] = value;
     }
     
     int get(int key) {
-        if(mp.find(key) == mp.end()) return -1;
         return mp[key];
     }
     
     void remove(int key) {
-        mp.erase(key);
+        mp[key] = -1;
     }
 };
 
