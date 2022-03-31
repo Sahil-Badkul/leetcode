3793a40e8463ecd4e52b77679f3dac7f7cb46266
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-03-31 09:44:25 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (80.23%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    /*
+    //Naive approach :- TC : O(n^2) && SC : O(n^2)
     vector<int> getRow(int rowIndex) {
         vector<vector<int>> pt;
         for(int i=0;i<=rowIndex;i++){
@@ -14,5 +16,18 @@ public:
             pt.push_back(temp);
         }
         return pt[rowIndex];
+    }
+    */
+    // Better approach :- TC : O(n^2) && SC : O(1)
+    vector<int> getRow(int n){
+        vector<int> res = {1}, cur = {1};
+        for(int i = 1; i <= n; i++){
+            res.push_back(1);
+            for(int j = 1; j < i; j++){
+                res[j] = cur[j-1] + cur[j];
+            }
+            cur = res;
+        }
+        return res;
     }
 };
