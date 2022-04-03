6d9f8109be6f816e871256c84008528b93ec63e4
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-04-03 06:16:17 +0530

    Time: 5 ms (56.22%), Space: 12 MB (93.15%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -7,6 +7,17 @@ public:
     }
     */
     // O(n) approach
+    /*
+    Approach :
+
+Step 1: Linearly traverse array from backward such that ith index value of the array is less than (i+1)th index value. Store that index in a variable.
+
+Step 2: If the index value received from step 1 is less than 0. This means the given input array is the largest lexicographical permutation. Hence, we will reverse the input array to get the minimum or starting permutation. Linearly traverse array from backward. Find an index that has a value greater than the previously found index. Store index another variable.
+
+Step 3: Swap values present in indices found in the above two steps.
+
+Step 4: Reverse array from index+1 where the index is found at step 1 till the end of the array.
+    */
     void nextPermutation(vector<int>& nums){
         int n = nums.size();
         int k,l;
