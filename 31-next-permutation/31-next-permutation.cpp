dd5fc942295a83b788751a0635d36f4f8050b610
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-04-03 06:15:20 +0530

    Time: 3 ms (87.24%), Space: 12.2 MB (37.25%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
+    /*
     void nextPermutation(vector<int>& nums) {
+        // cpp built in function.
         next_permutation(nums.begin(),nums.end());
+    }
+    */
+    // O(n) approach
+    void nextPermutation(vector<int>& nums){
+        int n = nums.size();
+        int k,l;
+        for(k = n-2; k >= 0; k--){
+            if(nums[k] < nums[k+1]){
+                break;
+            }
+        }
+        if(k < 0){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+        for(l = n-1; l >= 0; l--){
+            if(nums[l] > nums[k]) break;
+        }
+        swap(nums[l],nums[k]);
+        reverse(nums.begin()+k+1, nums.end());
     }
 };
