aeaa9304428d404566c16bbf7b97278051827a31
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-04-06 10:14:26 +0530

    Time: 1121 ms (24.49%), Space: 26.8 MB (5.44%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     int threeSumMulti(vector<int>& arr, int target) {
         unordered_map<int,int> mp;
         int n = arr.size(), res = 0, mod = 1e9+7;
@@ -10,5 +11,22 @@ public:
             }
         }
         return res;
+    }
+    */
+    int threeSumMulti(vector<int>& arr, int target) {
+        int n = arr.size(), mod = 1e9+7;;
+        long res = 0;
+        for(int i = 0; i < n; i++){
+            vector<int> count(101, 0);
+            for(int j = i+1; j < n; j++){
+                int k = target - arr[i] - arr[j];
+                if(k >= 0 && k <= 100 && count[k] > 0){
+                    res += count[k];
+                    res %= mod;
+                }
+                count[arr[j]]++;
+            }
+        }
+        return (int)res;
     }
 };
