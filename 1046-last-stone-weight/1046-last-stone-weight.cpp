65a267e9ad9c461194a031e9432eb65257098c43
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-04-07 07:04:19 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (34.27%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -5,22 +5,11 @@ public:
         for(auto &stone : stones){
             pq.push(stone);
         }
-        while(true){
-            if(pq.size() == 1) break;
-            int y = pq.top();
-            pq.pop();
-            int x = pq.top();
-            pq.pop();
-            if(y == x){
-                if(pq.empty()){
-                    return 0;
-                }
-                continue;
-            }
-            else{
-                pq.push(y-x);
-            }
+        while(pq.size() > 1){
+            int y = pq.top(); pq.pop();
+            int x = pq.top(); pq.pop();
+            if(y > x) pq.push(y-x);
         }
-        return pq.top();
+        return pq.empty() ? 0 : pq.top();
     }
 };
