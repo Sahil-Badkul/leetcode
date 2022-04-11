daba20c49efb8853a52f5cab45b56c122a2379f0
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-04-11 18:58:10 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (73.31%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    /*
+    // Queue 
     int findTheWinner(int n, int k) {
         queue<int> q;
         for(int i = 1; i <= n; i++){
@@ -16,5 +18,29 @@ public:
             q.pop();
         }
         return q.front();
+    }
+    // TC : O(n*k), SC:O(n);
+    */
+    /*
+    // Recursive
+    int helper(int n, int k){
+        if(n == 1) return 0;
+        return (helper(n-1, k) + k) % n;
+    }
+    int findTheWinner(int n, int k){
+        return helper(n,k)+1;
+    }
+    //TC : O(n), SC : O(n);
+    */
+    //Iterative
+    int helper(int n, int k){
+        int ans = 0;
+        for(int i = 1; i <= n; i++){
+            ans = (ans + k) % i;
+        }
+        return ans;
+    }
+    int findTheWinner(int n, int k){
+        return helper(n,k)+1; // +1, bcaz index are 0 based
     }
 };
