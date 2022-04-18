b66ea28314119982700dd92a9ad7acc18e07daf3
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-04-18 20:07:34 +0530

    Time: 235 ms (78.54%), Space: 62.5 MB (70.54%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
-    float distance(int x1, int x2, int y1, int y2){
-        return sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
+    int distance(int x,int y){
+        return x*x + y*y;
     }
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        vector<pair<float, int>> cal;
+        priority_queue<pair<int, int>> cal;
         for(int i = 0; i < points.size(); i++){
-            vector<int> v = points[i];
-            auto dist = distance(v[0], 0, v[1], 0);
-            cal.push_back({dist, i});
+            pair<int,int> point = {distance(points[i][0],points[i][1]), i};
+            if(cal.size() < k){
+                cal.push(point);
+            }else if(point.first < cal.top().first){
+                cal.pop();
+                cal.push(point);
+            }
         }
-        sort(cal.begin(), cal.end());
         vector<vector<int>> ans;
-        for(auto &x : cal){
-            if(k-- == 0) break;
-            int idx = x.second;
+        while(!cal.empty()){
+            int idx = cal.top().second;
+            cal.pop();
             ans.push_back(points[idx]);
         }
         return ans;
     }
+    //TC : 
 };
