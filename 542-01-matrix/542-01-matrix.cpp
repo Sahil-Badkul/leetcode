5de31d8911068716403cc7a179dd4d3e2c5c4581
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-04-22 07:35:29 +0530

    Time: 81 ms (76.96%), Space: 30.4 MB (39.99%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -1,5 +1,41 @@
 class Solution {
 public:
+    int dx[4] = {1,0,-1,0};
+    int dy[4] = {0,-1,0,1};
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int row = mat.size();
+        if(row == 0) return {};
+        int col = mat[0].size();
+        vector<vector<int>> ans(row, vector<int>(col, -1));
+        queue<pair<int,int>> q;
+        // push all 0's idx in queue and update it as 0;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(mat[i][j] == 0){
+                    q.push({i,j});
+                    ans[i][j] = 0;
+                }
+            }
+        }
+        while(!q.empty()){
+            auto point = q.front();
+            q.pop();
+            for(int i = 0; i < 4; i++){
+                int x = point.first +  dx[i];
+                int y = point.second + dy[i];
+                // is valid  --> ans[x][y] != -1 then it contain shortest path. mat[x][y] == 1 bcaz we are considering 1's only.
+                if(x >= 0 && y >= 0 && x < row && y < col && ans[x][y] == -1 && mat[x][y] == 1){
+                    ans[x][y] = ans[point.first][point.second] + 1;
+                    q.push({x,y});
+                }
+            }
+        }
+        return ans;
+    }
+};
+/*
+class Solution {
+public:
     bool isvalid(int i, int j, int m, int n){
         if(i < 0 || j < 0 || i >= n || j >= m) return false;
         return true;
@@ -44,3 +80,4 @@ public:
         return ans;
     }
 };
+*/
