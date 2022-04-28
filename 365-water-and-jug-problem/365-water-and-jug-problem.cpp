7d39905fb7141711de74509131b0c1003606deef
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-04-28 20:36:29 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (79.05%) - LeetHub

---
+++ 365-water-and-jug-problem/365-water-and-jug-problem.cpp
@@ -0,0 +1,6 @@
+class Solution {
+public:
+    bool canMeasureWater(int jug1Capacity, int jug2Capacity, int targetCapacity) {
+         return targetCapacity%__gcd(jug1Capacity,jug2Capacity) == 0 && targetCapacity<=jug1Capacity+jug2Capacity; 
+    }
+};
