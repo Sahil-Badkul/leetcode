b1484505264f9dc1ec15e67fc49938eabd1dac4c
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-04-29 18:22:37 +0530

    Time: 1423 ms (14.09%), Space: 32 MB (13.68%) - LeetHub

--- 127-word-ladder/127-word-ladder.cpp
+++ 127-word-ladder/127-word-ladder.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string> dict(wordList.begin(), wordList.end()); //O(1) searching time
-        queue<pair<string,int>> todo;
-        todo.push({beginWord, 1});
-        while (!todo.empty()) {
-                pair<string,int> p = todo.front();
-                string word = p.first;
-                int ladder = p.second;
-                todo.pop();
-                if (word == endWord) {
-                    return ladder;
+        unordered_set<string> list(wordList.begin(), wordList.end()); // to make search in O(1);
+        queue<pair<string,int>> q;
+        q.push({beginWord,1});
+        while(!q.empty()){
+            string str = q.front().first;
+            int ladder = q.front().second;
+            q.pop();
+            if(str == endWord){
+                return ladder;
+            }
+            list.erase(str);
+            for(int i = 0; i < str.size(); i++){
+                char c = str[i];
+                for(int k = 0; k < 26; k++){
+                    str[i] = 'a' + k;
+                    if(list.find(str) != list.end()){
+                        q.push({str, ladder+1});
+                    }
                 }
-                dict.erase(word);
-                for (int j = 0; j < word.size(); j++) {
-                    char c = word[j];
-                    for (int k = 0; k < 26; k++) {
-                        word[j] = 'a' + k;
-                        if (dict.find(word) != dict.end()) {
-                            todo.push({word, ladder+1});
-                        }
-                     }
-                    word[j] = c;
-                }    
-            
+                str[i] = c;
+            }
         }
         return 0;
     }
