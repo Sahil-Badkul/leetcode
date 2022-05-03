7baa5bd858764d33e3b4003f32a40291fd91932d
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-05-03 20:15:20 +0530

    Time: 485 ms (53.32%), Space: 0B (100.00%) - LeetHub

--- 183-customers-who-never-order/183-customers-who-never-order.sql
+++ 183-customers-who-never-order/183-customers-who-never-order.sql
@@ -2,6 +2,6 @@
 select name as Customers 
 from Customers
 where id NOT in (
-Select c.id
-from Customers as c inner join Orders as o
-on c.id = o.customerId)
+    Select customerId
+    From orders
+)
