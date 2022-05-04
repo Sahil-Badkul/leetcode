3f6c5b43c34b5fc752a1191dd317f1787a8c42dc
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-05-04 18:05:18 +0530

    Time: 514 ms (80.04%), Space: 0B (100.00%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -1,4 +1,11 @@
 # Write your MySQL query statement below
-select employee_id , if(employee_id % 2 = 1 and name not like 'M%', salary, 0) as bonus
-from Employees 
-order by employee_id
+# select employee_id , if(employee_id % 2 = 1 and name not like 'M%', salary, 0) as bonus
+# from Employees 
+# order by employee_id
+
+select employee_id, 
+    case 
+        when employee_id % 2 = 1 and name not like 'M%' then salary
+        else 0
+    end as bonus
+from Employees
