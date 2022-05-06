df169fce3d8ffca2f2acc557b7f7e18fc403406e
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-05-06 21:12:36 +0530

    Time: 946 ms (7.98%), Space: 0B (100.00%) - LeetHub

--- 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -1,8 +1,17 @@
-select employee_id 
-from employees 
-where employee_id Not in (select employee_id from salaries)
-Union 
-select employee_id
-from salaries
-where employee_id not in (select employee_id from employees)
-order by employee_id
+# select employee_id 
+# from employees 
+# where employee_id Not in (select employee_id from salaries)
+# Union 
+# select employee_id
+# from salaries
+# where employee_id not in (select employee_id from employees)
+# order by employee_id
+
+
+select t.employee_id
+from (select * from employees left join salaries using (employee_id)
+     Union
+     select * from employees right join salaries using (employee_id))
+     as t
+where t.salary is null or t.name is null
+order by t.employee_id
