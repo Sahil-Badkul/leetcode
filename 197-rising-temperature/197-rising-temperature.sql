17f3442a7c55d863a868d4ce28f09ba9292ec9ed
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-05-08 23:05:49 +0530

    Time: 548 ms (31.95%), Space: 0B (100.00%) - LeetHub

--- 197-rising-temperature/197-rising-temperature.sql
+++ 197-rising-temperature/197-rising-temperature.sql
@@ -1,4 +1,9 @@
-select weather.id as 'Id'
-from weather join weather w 
-on DATEDIFF(weather.recordDate, w.recordDate) = 1
-and weather.Temperature  > w.Temperature;
+# select weather.id as 'Id'
+# from weather join weather w 
+# on DATEDIFF(weather.recordDate, w.recordDate) = 1
+# and weather.Temperature  > w.Temperature;
+
+SELECT a.Id
+FROM Weather a join Weather b
+WHERE a.Temperature > b.Temperature
+AND DATEDIFF(a.Recorddate,b.Recorddate) = 1
