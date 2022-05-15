b9f9db47fd05100731d95a85f063d41503250405
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-05-15 15:49:37 +0530

    Time: 519 ms (29.13%), Space: 59.2 MB (94.18%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.cpp
+++ 1462-course-schedule-iv/1462-course-schedule-iv.cpp
@@ -1,5 +1,26 @@
 class Solution {
 public:
+    vector<bool> checkIfPrerequisite(int n, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
+        vector<vector<bool>> reachable(n, vector<bool>(n,0));
+        for(auto &p : prerequisites){
+            reachable[p[0]][p[1]] = 1;
+        }
+        vector<bool> result;
+        for(int k = 0; k < n; k++){
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < n; j++){
+                    reachable[i][j] = reachable[i][j] || (reachable[i][k] && reachable[k][j]);
+                }
+            }
+        }
+        for(auto &q : queries){
+            result.push_back(reachable[q[0]][q[1]]);
+        }
+        return result;
+    }
+};
+/*class Solution {
+public:
     vector<bool> checkIfPrerequisite(int n,vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
         if(queries.size() == 0){
             return {};
@@ -32,3 +53,4 @@ public:
         return false;
     }
 };
+*/
