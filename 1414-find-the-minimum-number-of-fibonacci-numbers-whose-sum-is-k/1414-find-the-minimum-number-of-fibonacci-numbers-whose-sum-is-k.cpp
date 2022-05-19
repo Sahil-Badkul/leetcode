cbabc3d9df3935b084b5924c7a649bec9f962718
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-05-19 07:33:17 +0530

    Time: 8 ms (14.66%), Space: 5.7 MB (98.28%) - LeetHub

---
+++ 1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k/1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+        int findMinFibonacciNumbers(int k) {
+        if (k < 2) return k;
+        int a = 1, b = 1;
+        while (b <= k) {
+            swap(a, b);
+            b += a;
+        }
+        return 1 + findMinFibonacciNumbers(k - a);
+    }
+};
