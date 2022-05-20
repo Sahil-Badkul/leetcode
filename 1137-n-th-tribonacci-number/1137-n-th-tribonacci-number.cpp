2d356d9c05901ab4064f441c1ce1e431d640f631
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-05-20 18:44:17 +0530

    Time: 3 ms (31.52%), Space: 5.9 MB (51.24%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,5 +1,20 @@
 class Solution {
 public:
+    int tribonacci(int n) {
+        int prev3 = 0, prev2 = 1, prev = 1;
+        if(n == 0) return 0;
+        if(n == 1 || n == 2) return 1;
+        int curr = 0;
+        for(int i = 3; i <= n; i++){
+            curr = prev + prev2 + prev3;
+            prev3 = prev2;
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
+    }
+    /*
+    // memoization
     int helper(int n, vector<long long> &dp){
         if(dp[n] != -1) return dp[n];
         else if(n == 0) return dp[0] = 0;
@@ -10,4 +25,5 @@ public:
         vector<long long> dp(n+1, -1);
         return helper(n, dp);
     }
+    */
 };
