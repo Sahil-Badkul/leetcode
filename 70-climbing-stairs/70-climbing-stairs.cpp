4564426929505e39632d84884522e4c22c56317c
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-05-22 05:48:37 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (15.67%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
+    int helper(int n, vector<int> &dp){
+        if(dp[n] != -1) return dp[n];
+        if(n == 0) return 0;
+        if(n == 1 || n == 2) return dp[n] = n;
+        int left = helper(n-2, dp);
+        int right = helper(n-1, dp);
+        return dp[n] = left + right;
+    }
     int climbStairs(int n) {
+        vector<int> dp(n+1, -1);
+        return helper(n, dp);
         // space optimization
         if(n <= 0) return 0;
         if(n == 1) return 1;
