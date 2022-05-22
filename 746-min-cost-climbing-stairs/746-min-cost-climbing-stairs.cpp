0bddc38199b3b78edff5035923848d322ecf2ac9
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-05-22 06:14:57 +0530

    Time: 8 ms (60.50%), Space: 13.8 MB (50.72%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    /*
+    // memoization.
     int helper(int idx,vector<int> &arr, vector<int> &dp){
         if(dp[idx] != -1) return dp[idx];
         if(idx == 0 || idx == 1) return arr[idx];
@@ -7,9 +9,21 @@ public:
         int right = arr[idx] + helper(idx-2, arr, dp);
         return dp[idx] = min(left, right);
     }
+    */
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
+        /*
+        // memoization
         vector<int> dp(n, -1);
         return min(helper(n-1, cost, dp), helper(n-2, cost, dp));
+        */
+        // Tabulation
+        vector<int> dp(n, 0);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for(int i = 2; i < n; i++){
+            dp[i] = cost[i] + min(dp[i-1],dp[i-2]);
+        }
+        return min(dp[n-1],dp[n-2]);
     }
 };
