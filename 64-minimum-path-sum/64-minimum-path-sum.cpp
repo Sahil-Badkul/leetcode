f7dc47c0f5252644c078bf33f7e50bcd79823c08
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-05-25 21:16:20 +0530

    Time: 26 ms (6.95%), Space: 10 MB (58.09%) - LeetHub

---
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    int memo(int i, int j, vector<vector<int>> &grid, vector<vector<int>> &dp){
+    if(i == 0 && j == 0) return grid[0][0];
+    if(i < 0 || j < 0) return 1e9;
+    if(dp[i][j] != -1) return dp[i][j];
+    int left = grid[i][j] + memo(i, j-1, grid, dp);
+    int up = grid[i][j] + memo(i-1, j, grid, dp);
+    return dp[i][j] = min(left, up);
+}
+int memoization(int n, int m, vector<vector<int>> &grid){
+    vector<vector<int>> dp(n, vector<int> (m, -1));
+    return memo(n-1, m-1, grid, dp);
+}
+int tabulation(int n, int m, vector<vector<int>> &grid){
+    vector<vector<int>> dp(n, vector<int>(m,0));
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            if(i == 0 && j == 0) dp[0][0] = grid[0][0];
+            else{
+                int up = grid[i][j], left = grid[i][j];
+                if(i > 0) up += dp[i-1][j];
+                else up += 1e9;
+                if(j > 0) left += dp[i][j-1];
+                else left += 1e9;
+                dp[i][j] = min(up, left);
+            }
+        }
+    }
+    return dp[n-1][m-1];
+}
+int spaceOptimize(int n, int m, vector<vector<int>> &grid){
+    vector<int> prev(m, 0);
+    for(int i = 0; i < n; i++){
+        vector<int> temp(m, 0);
+        for(int j = 0; j < m; j++){
+            if(i == 0 && j == 0) temp[0] = grid[0][0];
+            else{
+                int up = grid[i][j], left = grid[i][j];
+                if(i > 0) up += prev[j];
+                else up += 1e9;
+                if(j > 0) left += temp[j-1];
+                else left += 1e9;
+                temp[j] = min(up, left);
+            }
+        }
+        prev = temp;
+    }
+    return prev[m-1];
+}
+    int minPathSum(vector<vector<int>>& grid) {
+        int n = grid.size();
+    int m = grid[0].size();
+//     return memoization(n,m,grid);
+//     return tabulation(n, m, grid);
+    return spaceOptimize(n,m,grid);
+    }
+};
