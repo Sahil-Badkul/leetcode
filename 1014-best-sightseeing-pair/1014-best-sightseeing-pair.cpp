fd25d014d3cd7b43c4f669c6042f6196e7848133
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-05-26 18:39:40 +0530

    Time: 67 ms (62.41%), Space: 39.6 MB (25.79%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     int maxScoreSightseeingPair(vector<int>& values) {
+        int score = INT_MIN, n = values.size();
+        int dp[n];
+        dp[0] = values[0];
+        for(int i = 1; i < n; i++){
+            score = max(score, dp[i-1]+values[i]-i);
+            dp[i] = max(dp[i-1],values[i]+i);
+        }
+        return score;
+        /*
         // A[i]+i + A[j]-j 
         int prevBestIdx = 0;
         int ans = values[0];
@@ -13,5 +22,6 @@ public:
             }
         }
         return ans;
+        */
     }
 };
