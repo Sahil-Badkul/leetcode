45ec881ef492fb688ce9ab626ff089ce1057bb8e
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-05-26 07:14:05 +0530

    Time: 3 ms (97.79%), Space: 8.6 MB (68.63%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -25,9 +25,25 @@ int tabulation(vector<vector<int>>& triangle, int n){
     }
     return dp[0][0];
 }
+    int spaceOptimize(vector<vector<int>> &triangle, int n){
+    vector<int> prev(n, 0), curr(n, 0);
+    for(int j = 0; j < n; j++){
+        prev[j] = triangle[n-1][j];
+    }
+    for(int i = n-2; i >= 0; i--){
+        for(int j = i; j >= 0; j--){
+            int d = triangle[i][j] + prev[j];
+            int dg = triangle[i][j] + prev[j+1];
+            curr[j] = min(d, dg);
+        }
+        prev = curr;
+    }
+    return prev[0];
+    }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        //    return memoization(triangle, n);
-    return tabulation(triangle, n);
+        // return memoization(triangle, n);
+        // return tabulation(triangle, n);
+        return spaceOptimize(triangle, n);
     }
 };
