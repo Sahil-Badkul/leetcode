f94d71f4f5fc52d0dcbc2ee6d3ba70853b67e4fd
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-05-27 05:26:58 +0530

    Time: 15 ms (21.98%), Space: 15.2 MB (14.08%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -20,7 +20,27 @@ public:
         vector<vector<int>> dp(n, vector<int> (2, -1));
         return memo(0, 1, prices, dp);
     }
+    int tabulation(vector<int> &price){
+        int n = price.size();
+        vector<vector<int>> dp(n+1, vector<int> (2, 0));
+        
+        for(int idx = n-1; idx >= 0; idx--){
+            for(int buy = 0; buy < 2; buy++){
+                int profit = 0;
+                if(buy){
+                    profit = max(-price[idx] + dp[idx+1][0],
+                        0 + dp[idx+1][1]);
+                }else{
+                    profit = max(price[idx] + dp[idx+1][1],
+                           0 + dp[idx+1][0]);
+                }
+                dp[idx][buy] = profit;
+            }
+        }
+        return dp[0][1];
+    }
     int maxProfit(vector<int>& prices) {
-        return memoization(prices);
+        // return memoization(prices);
+        return tabulation(prices);
     }
 };
