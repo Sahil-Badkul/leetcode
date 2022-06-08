148a86e45727f4fb2e73af28d6b2894d053dffc2
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-06-08 22:45:42 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (72.15%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -11,8 +11,31 @@ public:
         }
         return dp[i] = memo(i+1, s, dp);
     }
-    int numDecodings(string s) {
+    int memoization(string &s){
         vector<int> dp(s.size(), -1);
         return memo(0, s, dp);
+    }
+    int tabulation(string &s){
+        int n = s.size();
+        vector<int> dp(n+1, 0);
+        if(n == 0 || s[0] == '0') return 0;
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++){
+            if(s[i-1] >= '1' && s[i-1] <= '9'){
+                dp[i] = dp[i-1];
+            }
+            if(s[i-2] == '1'){
+                dp[i] += dp[i-2];
+            }
+            else if(s[i-2] == '2' && (s[i-1] >= '0' && s[i-1] <= '6')){
+                dp[i] += dp[i-2];
+            }
+        }
+        return dp[n];
+    }
+    int numDecodings(string s) {
+        // return memoization(s);
+        return tabulation(s);
     }
 };
