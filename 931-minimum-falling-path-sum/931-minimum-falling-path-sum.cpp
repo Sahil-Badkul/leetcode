f2df81e90d88ce1fe6ec0a463b78818e36119722
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-06-09 07:16:07 +0530

    Time: 34 ms (9.71%), Space: 9.8 MB (77.99%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -44,8 +44,30 @@ public:
         }
         return mini;
     }
+    int spaceOptimize(vector<vector<int>> &matrix){
+        int n = matrix.size();
+        vector<int> prev(n, 0), curr(n, 0);
+        for(int i = 0; i < n; i++){
+            prev[i] = matrix[0][i];
+        }
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j < n; j++){
+                int up = prev[j];
+                int ld = j-1 < 0 ? INT_MAX : prev[j-1];
+                int rd = j+1 == n ? INT_MAX : prev[j+1];
+                curr[j] = matrix[i][j] + min(up, min(ld, rd));
+            }
+            prev = curr;
+        }
+        int mini = INT_MAX;
+        for(int i = 0; i < n; i++){
+            mini = min(mini, prev[i]);
+        }
+        return mini;
+    }
     int minFallingPathSum(vector<vector<int>>& matrix) {
         // return memoization(matrix);
-        return tabulation(matrix);
+        // return tabulation(matrix);
+        return spaceOptimize(matrix);
     }
 };
