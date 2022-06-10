eb51320e63622cb1849d79150d60a40c0a1efc49
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-06-10 09:58:09 +0530

    Time: 93 ms (13.95%), Space: 18.3 MB (43.11%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -19,7 +19,28 @@ public:
         vector<vector<int>> dp(n, vector<int> (x+1, -1));
         return memo(n-1, x, num, dp);
     }
+    int tabulation(vector<int> &num, int x)
+    {
+        int n = num.size();
+        vector<vector<int>> dp(n, vector<int> (x+1, 0));
+        for(int T = 0; T <= x; T++){
+            if(T % num[0] == 0) dp[0][T] = 1;
+            else dp[0][T] = 0;
+        }
+        for(int ind = 1; ind < n; ind++){
+            for(int T = 0; T <= x; T++){
+                int notTake = dp[ind-1][T];
+                int take = 0;
+                if(num[ind] <= T){
+                    take = dp[ind][T-num[ind]];
+                }
+                dp[ind][T] = (take + notTake);
+            }
+        }
+        return dp[n-1][x];
+    }
     int change(int amount, vector<int>& coins) {      
-        return memoization(coins, amount);
+        // return memoization(coins, amount);
+        return tabulation(coins, amount);
     }
 };
