65d4d46729b4c654c12a58f3b28cd153d02b8a72
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-06-12 18:53:58 +0530

    Time: 18 ms (58.17%), Space: 6.4 MB (94.92%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -46,8 +46,33 @@ public:
         }
         return dp[n][m];
     }
+    int spaceOptimize(string &s1, string &s2){
+        int n = s1.size(), m = s2.size();
+        vector<int> prev(m+1, 0), curr(m+1, 0);
+        // base cases
+        for(int j = 0; j <= m; j++){
+            prev[j] = j;   // if i exhausted
+        }
+        // changing parameter
+        for(int i = 1; i <= n; i++){
+            curr[0] = i;
+            for(int j = 1; j <= m; j++){
+                if(s1[i-1] == s2[j-1]){
+                    curr[j] = prev[j-1];
+                }else{
+                    int insert = 1 + curr[j-1];
+                    int del = 1 + prev[j];
+                    int replace = 1 + prev[j-1];
+                    curr[j] = min(insert, min(del, replace));
+                }
+            }
+            prev = curr;
+        }
+        return prev[m];
+    }
     int minDistance(string &s1, string &s2) {
         // return memoization(s1, s2);
-        return tabulation(s1, s2);
+        // return tabulation(s1, s2);
+        return spaceOptimize(s1, s2);
     }
 };
