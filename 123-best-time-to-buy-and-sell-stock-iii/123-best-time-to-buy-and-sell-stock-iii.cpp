0df73ce6c0d6a6314282374971ceec38090964ad
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-06-13 16:46:18 +0530

    Time: 1154 ms (10.98%), Space: 218 MB (11.06%) - LeetHub

---
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int memo(int idx, int buy, int cap, vector<int> &price, vector<vector<vector<int>>> &dp){
+        if(idx == price.size() || cap == 0){
+            return 0;
+        }
+        if(dp[idx][buy][cap] != -1) return dp[idx][buy][cap];
+        int profit = 0;
+        if(buy){
+            profit = max(-price[idx] + memo(idx+1, 0, cap, price, dp), 
+                        0 + memo(idx+1, 1, cap, price, dp));
+        }else{
+            profit = max(price[idx] + memo(idx+1, 1, cap-1, price, dp), 
+                        memo(idx+1, 0, cap, price, dp));
+        }
+        return dp[idx][buy][cap] = profit;
+    }
+    int memoization(vector<int> &prices){
+        int n = prices.size();
+        vector<vector<vector<int>>> dp(n, vector(2, vector<int> (3, -1)));
+        return memo(0, 1, 2, prices, dp);
+    }
+    int maxProfit(vector<int>& prices) {
+        return memoization(prices);
+    }
+};
