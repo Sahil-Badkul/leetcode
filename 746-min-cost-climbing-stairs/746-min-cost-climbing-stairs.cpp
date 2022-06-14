6d2a8f6c0c8e8e3d711ed52117971b5a3a789c74
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-06-14 06:27:13 +0530

    Time: 3 ms (96.59%), Space: 13.8 MB (49.60%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -1,7 +1,5 @@
 class Solution {
 public:
-    /*
-    // memoization.
     int helper(int idx,vector<int> &arr, vector<int> &dp){
         if(dp[idx] != -1) return dp[idx];
         if(idx == 0 || idx == 1) return arr[idx];
@@ -9,24 +7,25 @@ public:
         int right = arr[idx] + helper(idx-2, arr, dp);
         return dp[idx] = min(left, right);
     }
-    */
-    int minCostClimbingStairs(vector<int>& cost) {
-        int n = cost.size();
-        /*
+    int memoization(vector<int> &cost){
+        int n = cost.size();         
         // memoization
         vector<int> dp(n, -1);
         return min(helper(n-1, cost, dp), helper(n-2, cost, dp));
-        */
-        // Tabulation
-        /*
+    }
+    int tabulation(vector<int> &cost){
+        int n = cost.size();
+         // Tabulation
         vector<int> dp(n, 0);
         dp[0] = cost[0];
         dp[1] = cost[1];
         for(int i = 2; i < n; i++){
             dp[i] = cost[i] + min(dp[i-1],dp[i-2]);
         }
-        return min(dp[n-1],dp[n-2]);
-        */
+        return min(dp[n-1],dp[n-2]);        
+    }
+    int spaceOptimize(vector<int> &cost){  
+        int n  = cost.size();
         // space optimization
         int prev2 = cost[0];
         int prev = cost[1];
@@ -36,5 +35,10 @@ public:
             prev = curr;
         }
         return min(prev, prev2);
+    }
+    int minCostClimbingStairs(vector<int>& cost) {
+        // return memoization(cost);
+        return tabulation(cost);
+        // return spaceOptimize(cost);
     }
 };
