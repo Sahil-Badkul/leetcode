7545672ab307fdb893b7ae76e11416fc829498bf
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-06-16 09:14:25 +0530

    Time: 908 ms (8.85%), Space: 292.3 MB (6.32%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -9,9 +9,27 @@ public:
         }
         return dp[ind][prev_ind+1] = len;
     }
-    int lengthOfLIS(vector<int>& nums) {
+    int memoization(vector<int> &nums){ 
         int n = nums.size();
         vector<vector<int>> dp(n, vector<int> (n+1, -1));
         return memo(0, -1, nums, dp);
+    }
+    int tabulation(vector<int> &nums){
+        int n = nums.size();
+        vector<vector<int>> dp(n+1, vector<int> (n+1, 0));
+        for(int ind = n-1; ind >= 0; ind--){
+            for(int prev_ind = ind-1; prev_ind >= -1; prev_ind--){
+                int len = 0 + dp[ind+1][prev_ind+1];
+                if(prev_ind == -1 || nums[ind] > nums[prev_ind]){
+                    len = max(len, 1 + dp[ind+1][ind+1]);
+                }
+                dp[ind][prev_ind+1] = len;
+            }
+        }
+        return dp[0][-1+1];
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        // return memoization(nums);
+        return tabulation(nums);
     }
 };
