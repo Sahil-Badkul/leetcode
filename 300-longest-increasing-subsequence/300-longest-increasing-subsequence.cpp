1fc826deaaae5676d4ba556ee681eb31ab59d883
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-06-17 06:18:01 +0530

    Time: 12 ms (83.91%), Space: 10.5 MB (65.69%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -42,9 +42,25 @@ public:
         }
         return dp[-1+1];
     }
+    int binarySearch(vector<int> &nums){
+        vector<int> temp;
+        temp.push_back(nums[0]);
+        int len = 1;
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] > temp.back()){
+                temp.push_back(nums[i]);
+                len++;
+            }else{
+                int ind = lower_bound(temp.begin(), temp.end(), nums[i])-temp.begin();
+                temp[ind] = nums[i];
+            }
+        }
+        return len;
+    }
     int lengthOfLIS(vector<int>& nums) {
         // return memoization(nums);
         // return tabulation(nums);
-        return spaceOptimize(nums);
+        // return spaceOptimize(nums);
+        return binarySearch(nums);
     }
 };
