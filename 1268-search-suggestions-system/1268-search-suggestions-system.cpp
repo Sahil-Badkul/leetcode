37301dc3cdddddb0dfde417907a53ae8a714ec3f
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-06-19 21:24:22 +0530

    Time: 1013 ms (10.29%), Space: 664.4 MB (5.29%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -1,19 +1,65 @@
 class Solution {
 public:
-    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
-        sort(products.begin(), products.end());
-        vector<vector<string>> res;
-        int start, n = products.size();
-        string prefix;
-        for(auto &c : searchWord){
-            prefix += c;
-            start = lower_bound(products.begin(), products.end(), prefix) - products.begin();
-            
-            res.push_back({});
-            for(int i = start; i < min(start+3, n) && !products[i].compare(0, prefix.length(), prefix); i++) {
-                res.back().push_back(products[i]);
+    class Trie{
+    public: 
+    vector<Trie*> children;
+    bool isEnd;
+    
+    Trie(){
+        children = vector<Trie*> (26, nullptr);
+        isEnd = false;
+    }
+    
+    void addWord(string word){
+        Trie* node = this;
+        for(auto &c : word){
+            if(node->children[c - 'a'] == nullptr){
+                node->children[c - 'a'] = new Trie();
+            }
+            node = node->children[c - 'a'];
+        }
+        node->isEnd = true;
+    }
+    
+    void searchWord(vector<string> &res, string partial, int &count){
+        Trie* node =this;
+        if(count > 2) return;
+        if(node->isEnd == true){
+            res.push_back(partial);
+            count++;
+        }
+        for(int i = 0; i < 26; i++){
+            if(node->children[i] != nullptr){
+                partial += (i+'a');
+                node->children[i]->searchWord(res, partial, count);
+                partial.pop_back();
             }
         }
-        return res; 
+    }
+};
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+        Trie* trie = new Trie();
+        int n = searchWord.size();
+        vector<vector<string>> res;
+        for(string product : products){
+            trie->addWord(product);
+        }
+        string partial = "";
+        for(int i = 0; i < n; i++){
+            if(trie->children[searchWord[i] - 'a'] == nullptr){
+                while(i < n){
+                    res.push_back({});
+                    i++;
+                }
+            }else{
+                vector<string> cprefix;
+                partial += searchWord[i];
+                trie = trie->children[searchWord[i]-'a'];
+                int cnt = 0;
+                trie->searchWord(cprefix, partial, cnt);
+                res.push_back(cprefix);
+            }
+        }
+        return res;
     }
 };
