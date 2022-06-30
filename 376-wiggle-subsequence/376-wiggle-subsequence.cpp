badbfe598eaa834057f2aa6a28f1285e417042ff
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-06-30 13:10:30 +0530

    Time: 3 ms (68.95%), Space: 7 MB (75.46%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
+        /*
         // Gready
         int n = nums.size(), prevDiff = 0, currDiff, len = 1;
         for(int i = 1; i < n; i++){
@@ -11,5 +12,13 @@ public:
             }
         }
         return len;
+        */
+        // Dp
+        int n = nums.size(), up = 1, down = 1;
+        for(int i = 1; i < n; i++){
+            if(nums[i] - nums[i-1] > 0) up = 1 + down;
+            else if(nums[i] - nums[i-1] < 0) down = 1 + up;
+        }
+        return max(up, down);
     }
 };
