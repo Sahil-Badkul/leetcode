979094bbfc8c6304b49022b1f78071466b467d9a
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-07-05 14:44:12 +0530

    Time: 1029 ms (10.60%), Space: 6.6 MB (96.81%) - LeetHub

--- 686-repeated-string-match/686-repeated-string-match.cpp
+++ 686-repeated-string-match/686-repeated-string-match.cpp
@@ -4,9 +4,11 @@ public:
         int  n = a.size(), m = b.size();
         for(int i = 0; i < n; i++){
             int j = 0;
+            // if any ith char match then looping though string a
             while(j < m && a[(i+j)%n] == b[j]){
                 j++;
             }
+            // if all match then return from where matching started ith + jth index 
             if(j == m){
                 return (i+j-1)/n + 1;
             }
