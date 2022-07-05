c917c113f6da9d1f9a933e127fabe99e5bdea037
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-07-05 09:50:29 +0530

    Time: 7 ms (8.43%), Space: 6.1 MB (68.78%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.cpp
+++ 165-compare-version-numbers/165-compare-version-numbers.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     int compareVersion(string v1, string v2) {
+        // v1 & v2 size
         int n = v1.size(), m = v2.size();
+        // sum for comparing values b/w '.'
         int sum1 = 0, sum2 = 0;
+        // i & j are iterator
         int i = 0, j = 0;
         while(i < n || j < m){
+            // calculating sum of v1 b/w '.'
             while(i < n && v1[i] != '.'){
                 sum1 = sum1*10 + (v1[i] - '0');
                 i++;
             }
+            // calculating sum of v2 b/2 '.'
             while(j < m && v2[j] != '.'){
                 sum2 = sum2*10 + (v2[j] - '0');
                 j++;
@@ -19,7 +24,7 @@ public:
             }else if(sum1 > sum2){
                 return 1;
             }
-            // reinitialize sum;
+            // reinitialize sum & increasing pointers;
             sum1 = sum2 = 0;
             i++;
             j++;
