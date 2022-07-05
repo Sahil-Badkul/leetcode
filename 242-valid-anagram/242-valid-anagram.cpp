45af8a7dc4c3959822c629f556f5ad3d499025f5
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-07-05 07:40:58 +0530

    Time: 7 ms (87.33%), Space: 7.3 MB (77.90%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -2,9 +2,16 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         // sort     --> TC : O(SlogS) + O(TlogT), SC : O(1)
+        // sort(s.begin(), s.end());
+        // sort(t.begin(), t.end());
+        // return s == t;        
         // hash     --> TC : O(S) + O(T), SC : O(S)
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return s == t;        
+        vector<int> freq(26, 0);
+        for(auto &ch : s) freq[ch - 'a']++;
+        for(auto &ch : t) freq[ch - 'a']--;
+        for(auto &f : freq){
+            if(f != 0) return false;
+        }
+        return true;
     }
 };
