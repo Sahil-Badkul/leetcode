b3389e2d11afb6320f08b47ea15b6c08b959cee8
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-07-07 09:49:25 +0530

    Time: 4 ms (73.94%), Space: 7.2 MB (48.95%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -14,9 +14,33 @@ public:
         }
         return dp[i][j] = 0;
     }
-    bool isInterleave(string s1, string s2, string s3) {
+    int memoization(string s1, string s2, string s3){
         int n = s1.size(), m = s2.size(), k = s3.size();
         vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
         return f(0, 0, 0, s1, s2, s3, dp);
+    }
+    int tabulation(string s1, string s2, string s3){
+        int n = s1.size(), m = s2.size(), l = s3.size();
+        if(n + m != l) return 0;
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        for(int i = 0; i <= n; i++){
+            for(int j = 0; j <= m; j++){
+                if(i == 0 && j == 0){
+                    dp[i][j] = 1;
+                }
+                else if(i == 0){
+                    dp[i][j] = dp[i][j-1] && s2[j-1] == s3[i+j-1];
+                }else if(j == 0){
+                    dp[i][j] = dp[i-1][j] && s1[i-1] == s3[i+j-1];
+                }else{
+                    dp[i][j] = ((dp[i][j-1] && s2[j-1] == s3[i+j-1]) || (dp[i-1][j] && s1[i-1] == s3[i+j-1]));
+                }
+            }
+        }
+        return dp[n][m];
+    }
+    bool isInterleave(string s1, string s2, string s3) {
+        // return memoization(s1, s2, s3);
+        return tabulation(s1, s2, s3);
     }
 };
