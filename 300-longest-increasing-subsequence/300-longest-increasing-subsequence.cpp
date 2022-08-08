7031577f45ad463501aae60eabd23d50b13fd586
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-08-08 16:36:40 +0530

    Time: 18 ms (78.01%), Space: 10.5 MB (47.88%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -3,8 +3,11 @@ public:
     int memo(int ind, int prev_ind, vector<int> &nums, vector<vector<int>> &dp){
         if(ind == nums.size()) return 0;
         if(dp[ind][prev_ind+1] != -1) return dp[ind][prev_ind+1];
+        // not pick
         int len = 0 + memo(ind+1, prev_ind, nums, dp);
         if(prev_ind == -1 || nums[ind] > nums[prev_ind]){
+            // pick
+            // prev_ind --> ind
             len = max(len, 1 + memo(ind+1, ind, nums, dp));
         }
         return dp[ind][prev_ind+1] = len;
