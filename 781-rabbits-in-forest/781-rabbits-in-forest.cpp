122bac906cf872e483dbe01f04fe407fbea2f992
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-08-27 08:19:30 +0530

    Time: 8 ms (46.48%), Space: 8.6 MB (55.30%) - LeetHub

--- 781-rabbits-in-forest/781-rabbits-in-forest.cpp
+++ 781-rabbits-in-forest/781-rabbits-in-forest.cpp
@@ -1,23 +1,12 @@
 class Solution {
 public:
     int numRabbits(vector<int>& answers) {
+        // map to store freq;
         unordered_map<int,int> frq;
         for(auto &n : answers) frq[n]++;
         int rabbit = 0;
-        while(!frq.empty()){
-            auto p = *frq.begin();
-            int num = p.first;
-            int freq = p.second;
-            // cout<<num<<" "<<freq<<endl;
-            frq.erase(frq.begin());
-            if(freq > 0){
-                freq -= num+1;
-                rabbit += num+1;
-                // cout<<rabbit<<endl;
-            }else{
-                freq = 0;
-            }
-            if(freq > 0) frq[num] = freq;
+        for(auto &it : frq){
+            rabbit += (it.second + it.first) / (it.first+1) * (it.first+1);
         }
         return rabbit;
     }
