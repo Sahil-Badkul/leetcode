3e968bce664cf63754c6e480439d926fbff491fb
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-09-24 22:09:02 +0530

    Time: 7 ms (56.73%), Space: 6.5 MB (74.73%) - LeetHub

--- 299-bulls-and-cows/299-bulls-and-cows.cpp
+++ 299-bulls-and-cows/299-bulls-and-cows.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     string getHint(string s, string g) {
-        int se[256] = {0}, gr[256] = {0};
+        int se[10] = {0}, gr[10] = {0};
         int bull = 0, cow = 0;
         for(int i = 0; i < s.size(); i++){
             if(s[i] == g[i]) bull++;
-            se[s[i]]++;
+            se[s[i] - '0']++;
+            gr[g[i] - '0']++;
         }
-        for(int i = 0; i < g.size(); i++) gr[g[i]]++;
-        for(int i = 0; i < 256; i++) cow += min(se[i], gr[i]);
+        for(int i = 0; i < 10; i++) cow += min(se[i], gr[i]);
         cow -= bull;
         string ans = to_string(bull) + "A" + to_string(cow) + "B";
         return ans;
