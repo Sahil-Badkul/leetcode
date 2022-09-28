f8811447d865383f6b8c1fb22e9ced303f0a54d6
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-09-28 08:54:27 +0530

    Time: 25 ms (68.80%), Space: 7.8 MB (68.90%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-    string minWindow(string str, string t) {
-        unordered_map<char, int> mp;
+    string minWindow(string s, string t) {
+        map<char, int> mp;
         for(auto &ch : t) mp[ch]++;
-        int count = mp.size();
-        int i = 0, j = 0, n = str.size(), mini = 1e9;
-        string ans = "", temp = "";
-        while(i <= j && j < n){
-            if(mp.find(str[j]) != mp.end()){
-                mp[str[j]]--;
-                if(mp[str[j]] == 0) count--;
+        int mini = INT_MAX, count = mp.size(), i = 0, j = 0, n = s.size(), start = 0;
+        string ans = "";
+        while(j < n){
+            mp[s[j]]--;
+            if(mp[s[j]] == 0){
+                count--;
             }
             if(count == 0){
-                while(i <= j && count != 1){
-                    if(mp.find(str[i]) != mp.end()){
-                        mp[str[i]]++;
-                        if(mp[str[i]] > 0){ 
-                            count++;
-                            if(mini > j-i+1){
-                                mini = j-i+1;
-                                ans = str.substr(i, j-i+1);
-                            }
+                if(mini > j-i+1){
+                    mini = j-i+1;
+                    start = i;
+                }
+                while(count == 0){
+                    mp[s[i]]++;
+                    if(mp[s[i]] > 0){
+                        count++;
+                        if(mini > j-i+1){
+                            mini = j-i+1;
+                            start = i;
                         }
                     }
                     i++;
@@ -28,7 +29,6 @@ public:
             }
             j++;
         }
-        // cout<<mini<<endl;
-        return ans;
+        return mini == INT_MAX ? "" : s.substr(start, mini);
     }
 };
