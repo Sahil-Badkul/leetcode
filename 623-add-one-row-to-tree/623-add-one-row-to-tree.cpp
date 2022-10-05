f7cf3e316b0170589f47e8ebe0bd4de86949aafd
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-10-05 10:53:54 +0530

    Time: 43 ms (22.55%), Space: 25.4 MB (5.68%) - LeetHub

--- 623-add-one-row-to-tree/623-add-one-row-to-tree.cpp
+++ 623-add-one-row-to-tree/623-add-one-row-to-tree.cpp
@@ -1,37 +1,33 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void insert(TreeNode* root, int val, int curr_depth, int depth){
-        if(root == NULL) return;
-        if(curr_depth == depth-1){
-            TreeNode* temp = root->left;
-            root->left = new TreeNode(val);
-            root->left->left = temp;
-            temp = root->right;
-            root->right = new TreeNode(val);
-            root->right->right = temp;
-        }else{
-            insert(root->left, val, curr_depth+1, depth);
-            insert(root->right, val, curr_depth+1, depth);
-        }
-    }
     TreeNode* addOneRow(TreeNode* root, int val, int depth) {
         if(depth == 1){
-            TreeNode* curr = new TreeNode(val);
-            curr->left = root;
-            return curr;
+            TreeNode* newhead = new TreeNode(val);
+            newhead->left = root;
+            return newhead;
         }
-        insert(root, val, 1, depth);
+        stack<pair<TreeNode*, int>> s;
+        s.push({root, 1});
+        while(!s.empty()){
+            TreeNode* cur = s.top().first;
+            int curdep = s.top().second;
+            s.pop();
+            if(cur == nullptr) continue;
+            if(curdep == depth- 1){
+                TreeNode* templeft = cur->left;
+                TreeNode* tempright = cur->right;
+                TreeNode* newleft = new TreeNode(val);
+                TreeNode* newright = new TreeNode(val);
+                cur->left = newleft;
+                cur->right = newright;
+                newleft->left = templeft;
+                newright->right = tempright;
+            }
+            else{
+                s.push({cur->left, curdep+1});
+                s.push({cur->right, curdep+1});
+            }
+        }
         return root;
     }
 };
