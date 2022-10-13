2592caf95681fa4609340683e4661009f24e4dce
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-10-13 18:04:16 +0530

    Time: 8 ms (99.91%), Space: 17.9 MB (64.62%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int res = 0;
+        for(int i = 0; i < nums.size(); i++){
+            res ^= (i+1) ^ nums[i];
+        }
+        return res;
+    }
+};
