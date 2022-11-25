ab0d9610bcdd94b388222adeb584f40a5fe354df
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-11-25 19:05:07 +0530

    Time: 219 ms (56.50%), Space: 42.9 MB (56.67%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -1,67 +1,47 @@
 class Solution {
 public:
-      int sumSubarrayMins(vector<int>& A) {
-        
-        int n = A.size();
-        int MOD = 1e9 + 7;
-        vector<int> left(n), right(n);
-        
-        // for every i find the Next smaller element to left and right
-        
-        // Left
-        stack<int>st;
-        st.push(0);
-        left[0] = 1; // distance = 1, left not found, this is distance multiplied with num, so it can't be zero
-        for(int i=1; i<n; i++)
-        {
-            while(!st.empty() && A[i] < A[st.top()]) 
-                st.pop();
-            
-            if(st.empty()) 
-                left[i] = i+1; // total distance if less element not found = i+1
-            else 
-                left[i] = i - st.top(); // distance = i-st.top()
-            
-            st.push(i);
-            
+    // void print(vector<int> ar){
+    //     for(auto &x : ar) cout<<x<<" ";
+    //     cout<<endl;
+    // }
+    int sumSubarrayMins(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> next(n), prev(n);
+        for(int i = 0; i < n; i++){
+            prev[i] = i;
+            next[i] = n - i - 1;
         }
-        
-        while(st.size()) 
-            st.pop();
-        
-        // Right 
+        stack<int> st;
         st.push(n-1);
-        right[n-1] = 1; // distance = 1, right not found, this is distance multiplied with num, so it can't be zero
-        for(int i=n-2; i>=0; i--)
-        {
-            while(!st.empty() && A[i] <= A[st.top()]) 
+        for(int i = n-2; i >= 0; i--){
+            while(!st.empty() and arr[st.top()] >= arr[i]){
                 st.pop();
-            
-            if(st.empty()) 
-                right[i] = n-i; // distance
-            else 
-                right[i] = st.top()-i;
-            
+            }
+            if(!st.empty()){
+                next[i] = st.top()-i-1;
+            }
             st.push(i);
         }
-        
-        // total number of subarrays : (Left[i] * Right[i])
-        // total contribution in A[i] element in final answer : (Left * Right) * A[i] 
-        
-        for(int i=0; i<n; i++) 
-             cout << left[i] << " : " << right[i] << endl;
-        
-        // for each i, contribution is (Left * Right) * Element 
-        
-        int res = 0;
-        for(int i=0; i<n; i++)
-        {
-            long long prod = (left[i]*right[i])%MOD;
-            prod = (prod*A[i])%MOD;
-            res = (res + prod)%MOD;
+        while(!st.empty()) st.pop();
+        st.push(0);
+        for(int i = 1; i < n; i++){
+            while(!st.empty() and arr[st.top()] > arr[i]){
+                st.pop();
+            }
+            if(!st.empty()){
+                prev[i] = i-st.top()-1;
+            }
+            st.push(i);
         }
-        
-        return res%MOD;
+        // cout<<"next "<<endl;
+        // print(next);
+        // cout<<"prev "<<endl;
+        // print(prev);
+        long long ans = 0, mod = 1e9+7;
+        for(int i = 0; i < n; i++){
+            ans += (1l * arr[i] * (prev[i]+1) * (next[i]+1));
+            ans %= mod;
+        }
+        return ans;
     }
-    
 };
