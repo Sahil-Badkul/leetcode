02ed5e29b1053199a0d9d3d922edeb556dd1387f
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-12-27 06:56:20 +0530

    Time: 6 ms (77.65%), Space: 6.2 MB (82.88%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
+    // Not check because its check for palindrome
     // bool isPalindrome(string &s, int i, int j){
     //     while(i >= 0 && j < s.size()){
     //         if(s[i--] != s[j++]) return false;
     //     }
     //     return true;
     // }
+    // insteed of this just increase count while expanding
     int countSubstrings(string s) {
         int n = s.size(), cnt = 0;
         // for odd length
