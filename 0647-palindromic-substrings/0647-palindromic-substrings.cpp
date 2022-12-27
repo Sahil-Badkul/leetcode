834f07ed7fd3779c7bc2982166987898ec614163
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2022-12-27 06:54:19 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (82.88%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
-    int dp[1001][1001];
-    bool isPalindrome(string &s, int i, int j){
-        if(i >= j) return 1;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(s[i] != s[j]) return 0;
-        return dp[i][j] = isPalindrome(s, i+1, j-1);
-    }
+    // bool isPalindrome(string &s, int i, int j){
+    //     while(i >= 0 && j < s.size()){
+    //         if(s[i--] != s[j++]) return false;
+    //     }
+    //     return true;
+    // }
     int countSubstrings(string s) {
-        int n = s.size();
-        memset(dp, -1, sizeof(dp));
-        int cnt = 0;
+        int n = s.size(), cnt = 0;
+        // for odd length
         for(int i = 0; i < n; i++){
-            for(int j = i; j < n; j++){
-                if(isPalindrome(s, i, j))  cnt += 1;
+            int l = i, r = i;
+            while(l >= 0 && r < n && s[l] == s[r]){
+                cnt++;
+                l--;
+                r++;
+            }
+        }
+        // for even len
+        for(int i = 0; i < n; i++){
+            int l = i-1, r = i;
+            while(l >= 0 && r < n && s[l] == s[r]){
+                cnt++;
+                l--;
+                r++;
             }
         }
         return cnt;
