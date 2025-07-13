0e6dc6a6b2fde1a157c26a630d5915663712936e
Sahil Jain <82427658+Sahil-Badkul@users.noreply.github.com>
2025-07-13 12:58:30 +0530

    Time: 0 ms (100%), Space: 17.7 MB (96.01%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,13 +1,16 @@
 class Solution:
-    def makeLCP(self, first, second):
-        res = ""
-        firstLen, secondLen = len(first), len(second)
-        for i in range(min(firstLen, secondLen)):
-            if first[i] == second[i]:
-                res += first[i]
-            else:
-                break
-        return res
+    def makeLCP(self, prefix, word):
+        # res = ""
+        # firstLen, secondLen = len(first), len(second)
+        # for i in range(min(firstLen, secondLen)):
+        #     if first[i] == second[i]:
+        #         res += first[i]
+        #     else:
+        #         break
+        # return res
+        while word.find(prefix) != 0: # We will get 0 if there is prefix at start 0th idx
+            prefix = prefix[0 : len(prefix)-1]
+        return prefix
 
     def longestCommonPrefix(self, strs: List[str]) -> str:
         LCP = strs[0]
